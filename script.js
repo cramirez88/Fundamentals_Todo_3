7b489668339b0a57fa93e1ac1e8cf6b7d15031ad
Christian Ramirez <christian.r0602@gmail.com>
2022-07-06 09:42:33 -0500

    biggie size

---
+++ script.js
@@ -0,0 +1,13 @@
+// Biggie Size
+// Given an array, write a function that changes all positive numbers in the array to “big”. Example: makeItBig([-1,3,5,-5]) returns that same array, changed to [-1,"big","big",-5].
+
+// function makeBig(arr){
+  
+//   for (let i = 0; i < arr.length; i++){
+//     if (arr[i] > 0){
+//       arr[i] = 'Big'
+//     }
+//   }
+//   return arr
+// }
+// console.log(makeBig([4,-2,-3,3,2]))
