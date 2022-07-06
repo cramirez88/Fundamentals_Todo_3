5e5d042056a49c1f03c1a5de594542b877421076
Christian Ramirez <christian.r0602@gmail.com>
2022-07-06 10:05:07 -0500

    printLow return High

--- script.js
+++ script.js
@@ -11,3 +11,25 @@
 //   return arr
 // }
 // console.log(makeBig([4,-2,-3,3,2]))
+
+// Print Low, Return High
+// Create a function that takes an array of numbers. The function should print the lowest value in the array, and return the highest value in the array.
+
+
+// function printLowReturnHigh(arr){
+//   let lowArr = arr[0]
+//   let highArr = arr[0]
+  
+//   for(let i = 0; i < arr.length; i++){
+//     if(arr[i] < lowArr){
+//       lowArr = arr[i]
+//       console.log(lowArr)
+//     }
+//     if(arr[i] > highArr){
+//       highArr = arr[i]
+//     }
+//   }
+//     return [highArr, lowArr]
+// }
+
+// console.log(printLowReturnHigh([1,2,3,2,5,3,-1,-50]))
