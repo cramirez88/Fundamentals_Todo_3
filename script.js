59eef947fa383b2f1690483e608ed31d6dd1e2fe
Christian Ramirez <christian.r0602@gmail.com>
2022-07-06 22:40:37 -0500

    swap toward center

--- script.js
+++ script.js
@@ -209,6 +209,19 @@
 // console.log(alwaysHungry(['food', 'hi', 'la', 'not food']))
 
 
+// Swap Toward the Center
+// Given array, swap first and last, third and third-tolast, etc. Input[true,42,"Ada",2,"pizza"] becomes ["pizza",42,"Ada",2,true].  Change [1,2,3,4,5,6] to [6,2,4,3,5,1].
+
+// function swap(arr){
+//   let newArr = []
+//   for (let i = arr.length -1; i >= 0; i--){
+//     newArr.push(arr[i])
+//   }
+//   return newArr
+// }
+
+// console.log(swap([1,'hi', 'bacon', 'coding', 'dojo']))
+
 
 
 
